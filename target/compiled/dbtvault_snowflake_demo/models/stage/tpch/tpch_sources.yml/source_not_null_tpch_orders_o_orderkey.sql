dc7c7e70acd5a396edfeef81af1b5e429dd68396
tattwa.neoace@gmail.com <tattwa.neoace@gmail.com>
2022-06-24 02:11:47 +0000

    test added

---
+++ target/compiled/dbtvault_snowflake_demo/models/stage/tpch/tpch_sources.yml/source_not_null_tpch_orders_o_orderkey.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from snowflake_sample_data.tpch_sf1.orders
+where o_orderkey is null
+
+
---
+++ target/run/dbtvault_snowflake_demo/models/stage/tpch/tpch_sources.yml/source_not_null_tpch_orders_o_orderkey.sql
@@ -0,0 +1,17 @@
+select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      
+    
+    
+
+select *
+from snowflake_sample_data.tpch_sf1.orders
+where o_orderkey is null
+
+
+
+      
+    ) dbt_internal_test
